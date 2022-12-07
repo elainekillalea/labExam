569218eec7c6c6ebe9f987dee122db653c1bbf5a
g00370088 <g00370088@atu.ie>
2022-12-07 10:37:37 +0000

    Updated EmployeeList

--- src/test/java/EmployeeListTest.java
+++ src/test/java/EmployeeListTest.java
@@ -1,5 +1,6 @@
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -8,7 +9,16 @@ class EmployeeListTest {
     @BeforeEach
     void setUp() {
     }
+    @Test
+    void testPass(){
 
+    }
+
+    @Test
+    void testFail(){
+        //Exception exception = assertThrows(Exception.class, () -> e1.setName("Jane"));
+       // assertEquals("Name must be at least 5 characters", exception.getMessage());
+    }
     @AfterEach
     void tearDown() {
     }
